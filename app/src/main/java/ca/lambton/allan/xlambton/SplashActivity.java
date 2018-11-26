9ad3bb01f49bacad3b3cd20be6eb770021fa5374
Allan Im <allan@vogle.com>
2018-11-25 19:16:58 -0500

    Add Splash

---
+++ app/src/main/java/ca/lambton/allan/xlambton/SplashActivity.java
@@ -0,0 +1,24 @@
+package ca.lambton.allan.xlambton;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+}
