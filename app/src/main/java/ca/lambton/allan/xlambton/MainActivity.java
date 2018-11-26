dced76fec294ce220b39a013b05917af78ce62c2
Allan Im <allan@vogle.com>
2018-11-25 23:41:03 -0500

    Update main activity

--- app/src/main/java/ca/lambton/allan/xlambton/MainActivity.java
+++ app/src/main/java/ca/lambton/allan/xlambton/MainActivity.java
@@ -2,6 +2,7 @@ package ca.lambton.allan.xlambton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // agents list
+        Button menuList = findViewById(R.id.menu_list);
+        menuList.setOnClickListener(v -> {
+
+        });
+
+        // search agent
+        Button menuSearch = findViewById(R.id.menu_search);
+        menuSearch.setOnClickListener(v -> {
+
+        });
+
+        // add agent
+        Button menuAdd = findViewById(R.id.menu_add);
+        menuAdd.setOnClickListener(v -> {
+
+        });
+
     }
 }
